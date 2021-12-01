2dd5941a1ee45ffdf2b1a7ca80a3a84271bd7437
Marcus Skriver <marcus@familienskriver.dk>
2021-12-01 15:18:25 +0100

    Finished DepthChanges

---
+++ Day 1 - DepthChanges/DepthChanges.py
@@ -0,0 +1,25 @@
+binaryArray = []
+with open('Day 1 - DepthChanges/numberfile.txt', 'rb') as f:
+    binaryArray = list(f)
+
+intArray = []
+for binary in binaryArray:
+    intArray.append(int(binary))
+
+prevInt = 0
+increaseCount = 0
+for int in intArray:
+    if (prevInt == 0):
+        prevInt = int
+    else:
+        changes = ''
+        if (int - prevInt > 0):
+            changes = ' (Increased)'
+            increaseCount += 1
+        else:
+            changes = ' (Decreased)'
+        print(str(int) + changes)
+        prevInt = int
+
+
+print('Depth increased ' + str(increaseCount) + ' times.')
