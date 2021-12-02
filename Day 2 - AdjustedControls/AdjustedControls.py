e8ebfd07c41e5b03a3dfe0fb7d8a0b5da5d8d2b7
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2021-12-02 11:46:42 +0100

    Finished AdjustedControls

---
+++ Day 2 - AdjustedControls/AdjustedControls.py
@@ -0,0 +1,20 @@
+tupleArray = []
+with open('Day 2 - Controls/numberfile.txt', 'rt') as f:
+    for line in f.readlines():
+        var = line.split()
+        tupleArray.append((var[0], int(var[1])))
+
+hozPos = 0
+depth = 0
+aim = 0
+for command in tupleArray:
+    match command[0]:
+        case 'forward':
+            hozPos += command[1]
+            depth += aim * command[1]
+        case 'down':
+            aim += command[1]
+        case 'up':
+            aim -= command[1]
+
+print(hozPos * depth)
