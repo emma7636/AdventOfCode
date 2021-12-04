e9b2a92f377b0afe34e2c0e383bad12f8a45ba09
Marcus Skriver <marcus@familienskriver.dk>
2021-12-04 22:29:27 +0100

    Completed LoseBingo

--- Day 4 - LoseBingo/LoseBingo.py
+++ Day 4 - LoseBingo/LoseBingo.py
@@ -1,3 +1,5 @@
+import numpy as numpy
+
 numberArray = []
 plateArray = []
 with open('Day 4 - WinBingo/numberfile.txt', 'rt') as f:
@@ -17,3 +19,60 @@ with open('Day 4 - WinBingo/numberfile.txt', 'rt') as f:
                 currentBoard[i].append(int(number))
             i += 1
     plateArray.append(currentBoard)
+
+def is_in(x, nested):
+    result = False
+    if not isinstance(nested, (tuple, list)):         
+        return result
+    for item in nested:
+        if x == item:
+            result = True
+        else:
+            result = result or is_in(x, item)
+        if result:
+            return True
+    return result
+
+losingPlates = []
+spentNumbers = []
+exit = False
+for number in numberArray:
+    spentNumbers.append(number)
+    for plate in plateArray:
+        for row in plate:
+            if all([row[0] in spentNumbers, row[1] in spentNumbers, row[2] in spentNumbers, row[3] in spentNumbers, row[4] in spentNumbers]):
+                if plate in plateArray:
+                    if(len(losingPlates) > 0):
+                        losingPlates.append(plate)
+                        plateArray.remove(plate)
+                    else:
+                        losingPlates.append(plate)
+                        plateArray.remove(plate)
+            
+            j = 0
+            while(j < 5):
+                if all([plate[0][j] in spentNumbers, plate[1][j] in spentNumbers, plate[2][j] in spentNumbers, plate[3][j] in spentNumbers, plate[4][j] in spentNumbers]):
+                    if plate in plateArray:
+                        if (len(losingPlates) > 0):
+                            losingPlates.append(plate)
+                            plateArray.remove(plate)
+                        else:
+                            losingPlates.append(plate)
+                            plateArray.remove(plate)
+                j += 1
+    if len(plateArray) == 0:
+        break
+
+print(losingPlates[len(losingPlates) - 1])
+
+i = 0
+sum = 0
+while (i < 5):
+    j = 0
+    while (j < 5):
+        if losingPlates[len(losingPlates) - 1][i][j] not in spentNumbers:
+            sum += losingPlates[len(losingPlates) - 1][i][j]
+        j += 1
+    i += 1
+
+print(sum * spentNumbers[len(spentNumbers) - 1])
