6256bad62e328e1e7f56b60bf4b0d890569e4902
Marcus Skriver <marcus@familienskriver.dk>
2022-12-01 15:22:18 +0100

    Finished CalorieCounting

---
+++ 2022/Day 1 - CalorieCounting/CalorieCounting.py
@@ -0,0 +1,23 @@
+calorieArr = []
+with open('2022/Day 1 - CalorieCounting/numberfile.txt', 'rt') as f:
+    for line in f:
+        line = line.split('\n')[0]
+        calorieArr.append(line)
+
+def sum(array):
+    temp = 0
+    for number in array:
+        temp += int(number)
+    return temp
+
+tempArr = []
+elfArr = []
+for line in calorieArr:
+    if line:
+        tempArr.append(line)
+    else:
+        elfArr.append(sum(tempArr))
+        tempArr.clear()
+
+elfArr.sort(reverse=True)
+print(elfArr[0])
