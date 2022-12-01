a7bbe896a3caac5d841ddd7aedebfff85e4357ba
Marcus Skriver <marcus@familienskriver.dk>
2022-12-01 14:05:00 +0100

    Started AdventOfCode 2022

---
+++ 2022/day_1_calorie_counting/target/debug/deps/Day1_CalorieCounting.d
@@ -0,0 +1,5 @@
+D:\OneDrive\Dokumenter\AdventOfCode\2022\Day1-CalorieCounting\target\debug\deps\Day1_CalorieCounting.exe: src\main.rs
+
+D:\OneDrive\Dokumenter\AdventOfCode\2022\Day1-CalorieCounting\target\debug\deps\Day1_CalorieCounting.d: src\main.rs
+
+src\main.rs:
